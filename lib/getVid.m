f25b1546555b949e953193e40b90b684adbcfbe8
Valentin <valentin.burger@informatik.uni-wuerzburg.de>
2014-03-04 14:23:33 +0100

    first pre-version of framework

--- lib/getVid.m
+++ lib/getVid.m
@@ -1,12 +1,12 @@
 function vid=getVid(uid, GV, H, wall)
-% TODO
-% niche - popular
-% propagation size dependent on clustering coefficient ~ 150*exp(-5*x)
-% TODO recency
-ind = geornd(0.4)+1;
-if (isnan(wall(ind,uid)))
-    vid = randi(size(GV,1));
-else
-    vid = wall(ind,uid);
-end
+    % TODO
+    % niche - popular
+    % propagation size dependent on clustering coefficient ~ 150*exp(-5*x)
+    % TODO recency
+    ind = geornd(0.4)+1;
+    if (isnan(wall(ind,uid)))
+        vid = randi(size(GV,1));
+    else
+        vid = wall(ind,uid);
+    end
 end
