3b1a5c81217fcf543bdb94f39cae4c520d35b9f6
rplc <rplc.alex@gmail.com>
2014-04-23 21:02:24 +0200

    added medians of snm

--- lib/getVideo.m
+++ lib/getVideo.m
@@ -27,6 +27,12 @@ SNM = 4;
             % George you can implement your model here
         case SNM
             %http://www.sigcomm.org/sites/default/files/ccr/papers/2013/October/2541468-2541470.pdf
+            %Percentage of videos - lifeSpan (days) - NumberOfRequests
+            % class 1: 3.6025 - 1.0825 - 78.15
+            % class 2: 5.3325 - 3.3575 - 45.975
+            % class 3: 3.3175 - 6.395 - 57
+            % class 4: 5.305 - 10.665 - 38.45
+            % class 5: 82.4525 - 24.17 - 26.225
     end
     
 end
