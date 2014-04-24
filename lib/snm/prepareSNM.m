228a8fc6427a267b710c66fbbf19e554d18988bc
Alexander Strehler <rplc.alex@gmail.com>
2014-04-24 23:27:33 +0200

    some functionality to prepare snm data set

--- lib/snm/prepareSNM.m
+++ lib/snm/prepareSNM.m
@@ -3,5 +3,23 @@ function snm=prepareSNM(par)
 %par.snm.classes.perc
 %par.snm.classes.lifeSpan
 %par.snm.classes.requests
+%par.nvids
 
+maxVal = sum(par.snm.classes.perc);
+cumSum = cumsum(par.snm.classes.perc);
+
+snm.videos = NaN(par.nvids,1);
+snm.seen = [];
+snm.timeStamps = zeros(par.nvids,1);
+
+for i=1:par.nvids
+    rnd = rand() * maxVal;
+    
+    snm.videos(i) = find(rnd <= cumSum, 1, 'first'); %find the category for random number
+end
+
+%TODO object:
+%snm.vids = videoID -> Class
+%snm.seen = [videoID] -> snm.vids[snm.seen] -> array mit nur gesehen
+%snm.timeStamp = videoID -> timeStamp (initialWatched)
 end
