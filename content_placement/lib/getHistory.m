475c30692bb5adf57b89a9c8bb2669da81aeb88f
Valentin <valentin.burger@informatik.uni-wuerzburg.de>
2015-01-16 17:21:06 +0100

    bw constraints added

--- content_placement/lib/getHistory.m
+++
@@ -1,8 +0,0 @@
-function h = getHistory(uid, stats, timelag)
-
-if nargin < 3
-    index = stats.uid == uid & ~isnan(stats.watch);
-    h = stats.watch(index);
-end
-
-end
--- content_placement/lib/updateWall.m
+++
@@ -1,7 +0,0 @@
-function wall = updateWall(GF, wall, uid, vid)
-    friends = find(GF(uid,:));
-    for i=friends
-        wall(i,2:end) = wall(i,1:end-1);
-        wall(i,1) = vid;
-    end
-end
